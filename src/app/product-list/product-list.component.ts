2bac54e54dd2d703aafd39f58a316430358098da
Weixin Qi <s4590288@gmail.com>
2022-06-11 22:36:47 +1000

    feat: 🎸 An angular official document start up tutorial
    A start up project to learn the basic angular knowledge from the
    official angular document

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, AfterContentInit, AfterContentChecked } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements AfterContentInit, AfterContentChecked {
+  ngAfterContentChecked(): void {
+    console.log('ContentCheck trigger');
+  }
+  ngAfterContentInit(): void {
+    console.log('ContentInit trigger');
+  }
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
